45a5c8f98df9708015e20a91ef271f922b2b7fe3
ASHISH <ashish2829003@gmail.com>
2020-09-02 17:40:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const getDownloadUrl = require('facebook-video-downloader');
+const app = express();
+app.use(cors());
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+// var path = __dirname;
+// console.log(path);
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname+'/page.html');
+});
+app.get('/download', async (req,res) => {
+var URL = req.query.URL;
+console.log(URL);
+res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+ytdl(URL, {
+    format: 'mp4'
+    }).pipe(res);
+
+// let info = await ytdl.getInfo(URL);
+// let format = ytdl.chooseFormat(info.formats);
+// console.log('Format found!', format);
+
+// let info1 = await ytdl.getInfo(URL);
+// let audioFormats = ytdl.filterFormats(info1.formats, 'audioonly');
+// console.log('Formats with only audio: ' + audioFormats);
+
+
+ 
+
+
+});
